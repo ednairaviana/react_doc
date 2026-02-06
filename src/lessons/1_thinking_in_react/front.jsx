c14e060d7bf49caa70353d95c67ca8a74311e27b
Ednaira Viana <ednaira@aldea.studio>
2026-02-05 23:18:24 -0300

    feat: Finish Lesson

--- src/lessons/1_thinking_in_react/front.jsx
+++ src/lessons/1_thinking_in_react/front.jsx
@@ -1,35 +1,70 @@
+import { useState } from "react";
 import movies from "./data";
 
-function SearchBar() {
+function SearchBar({
+  isWatchedOnly,
+  onIsWatchedOnlyChange,
+  filterText,
+  onFilterTextChange,
+}) {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col gap-1">
         <label htmlFor="search">Search Movie</label>
-        <input id="search" type="text" placeholder="Search..." />
+        <input
+          id="search"
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+          onChange={(e) => onFilterTextChange(e.target.value)}
+        />
       </div>
-      <div className="flex flex-row gap-1">
+      <div className="flex flex-row gap-5">
         <label htmlFor="search">Show only watched movies</label>
-        <input id="search" type="checkbox" value="only_watched" />
+        <input
+          id="search"
+          type="checkbox"
+          onChange={(e) => onIsWatchedOnlyChange(e.target.checked)}
+          checked={isWatchedOnly}
+        />
       </div>
     </div>
   );
 }
 
-function MoviesList() {
+function MoviesList({ isWatchedOnly, filterText }) {
   return (
     <ul>
       {movies.map((movie) => {
-        return <li key={movie.id}>{movie.title}</li>;
+        if (isWatchedOnly && !movie.watched) return;
+        if (
+          movie.title.toLowerCase().indexOf(filterText.toLowerCase()) === -1
+        ) {
+          return;
+        }
+        return (
+          <li key={movie.id} className={movie.watched && "text-green-600"}>
+            {movie.title}
+          </li>
+        );
       })}
     </ul>
   );
 }
 
 function MovieView() {
+  const [isWatchedOnly, setIsWatchedOnly] = useState(false);
+  const [filterText, setFilterText] = useState("");
+
   return (
     <div className="flex flex-col gap-8">
-      <SearchBar />
-      <MoviesList />
+      <SearchBar
+        isWatchedOnly={isWatchedOnly}
+        onIsWatchedOnlyChange={setIsWatchedOnly}
+        filterText={filterText}
+        onFilterTextChange={setFilterText}
+      />
+      <MoviesList isWatchedOnly={isWatchedOnly} filterText={filterText} />
     </div>
   );
 }
