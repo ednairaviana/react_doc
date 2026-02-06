6774ee1cbb9549688f8bf652dc437af98ea3da52
Ednaira Viana <ednaira@aldea.studio>
2026-02-05 22:16:11 -0300

    fix: Fix not re-rendering view (AI support)

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -4,39 +4,44 @@ const lessons = import.meta.glob("../lessons/**/front.jsx", {
   eager: true,
 });
 
-function AsideItems({ onSetView }) {
-  function formatTitle(path) {
-    const slug = path.split("/").slice(-2, -1)[0];
-    return slug.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase());
-  }
+// -------- utils --------
+function formatTitle(path) {
+  const slug = path.split("/").slice(-2, -1)[0];
 
-  function handleSetView(module) {
-    onSetView(module.default);
-  }
-
-  return Object.entries(lessons).map(([path, module]) => {
-    const title = formatTitle(path);
-
-    return (
-      <li>
-        <button onClick={() => handleSetView(module)}>{title}</button>
-      </li>
-    );
-  });
+  return slug.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase());
 }
 
-function HomeView() {
-  const [view, setView] = useState(Object.values(lessons)[0].default);
-
+// -------- Aside --------
+function AsideItems({ lessons, onSelect }) {
   return (
     <>
+      {Object.keys(lessons).map((path) => (
+        <li key={path}>
+          <button onClick={() => onSelect(path)}>{formatTitle(path)}</button>
+        </li>
+      ))}
+    </>
+  );
+}
+
+// -------- Page --------
+function HomeView() {
+  const lessonPaths = Object.keys(lessons);
+  const [activePath, setActivePath] = useState(lessonPaths[0]);
+  const ActiveView = lessons[activePath].default;
+
+  return (
+    <div className="container grid grid-cols-[1fr_2fr]">
       <aside>
         <ul>
-          <AsideItems onSetView={setView}></AsideItems>
+          <AsideItems lessons={lessons} onSelect={setActivePath} />
         </ul>
       </aside>
-      <section>{view}</section>
-    </>
+
+      <section>
+        <ActiveView />
+      </section>
+    </div>
   );
 }
 
