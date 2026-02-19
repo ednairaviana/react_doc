514798c4082b5131ddc20bee0a306cd26fbd2e7f
Ednaira Viana <ednaira@aldea.studio>
2026-02-19 14:02:37 -0300

    feat: create counter panel functionalities and basic ui

--- src/lessons/2_Interactive Counter Panel/front.jsx
+++ src/lessons/2_Interactive Counter Panel/front.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
 function View() {
+  const [counter, setCounter] = useState(0);
+
+  function handleCounterChange(operation) {
+    switch (operation) {
+      case "+":
+        setCounter(counter + 1);
+        break;
+      case "-":
+        setCounter(counter - 1);
+        break;
+      case "reset":
+        setCounter(0);
+        break;
+    }
+  }
+
   return (
-    <div>
-      this is a copy aaa
-      <div></div>
+    <div className="flex flex-col gap-8">
+      <div className="grid grid-cols-3">
+        <button onClick={() => handleCounterChange("+")}>+</button>
+        <button onClick={() => handleCounterChange("-")}>-</button>
+        <button onClick={() => handleCounterChange("reset")}>Reset</button>
+      </div>
+
+      <div> Counter: {counter}</div>
     </div>
   );
 }
